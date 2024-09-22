71310c1dea61ef0103bc66e8c2cf723be9002854
Ravi <raviprakashboss87@gmail.com>
2024-09-22 15:57:11 +0530

    //Final LAb06 part 02

--- Group06_Part02/main.c
+++ Group06_Part02/main.c
@@ -28,21 +28,34 @@ void button_read (void);
 
 volatile int duty = 50;                    // Duty cycle in percentage
 
-void GPIOFHandler(void)
-{
-    if(GPIO_PORTF_MIS_R == 0x10){           // Interrupt from SW1
-        button_read ();
-        //GPIO_PORTF_DATA_R ^= 0x08;          // Toggle data in PF1 (green led)
-    }
+void Sys_Port_Initialisation(void){
 
-    /*if(GPIO_PORTF_MIS_R == 0x01){           // Interrupt from SW2
-        //GPIO_PORTF_DATA_R ^= 0x02;          // Toggle data in PF1 (red led)
-        if(duty != 0) duty = duty-5;        // Decreasing duty cycle by 5 %
-        if(duty < 0) duty = 0;
-        PWM1_3_CMPA_R = (time_period * duty) / 100;
-    }*/
+    // PORTF, PF7-PF0, PF4-SW1, PF3-green, PF2-blue, PF1-red, PF0-SW2
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;   // Enable clock for Port F
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port F
+    GPIO_PORTF_CR_R = 0x1f;                 // Commit changes,1-enable (PF7-PF0 = 00011111)
+    GPIO_PORTF_DEN_R = 0x1f;                // Digital function enable, 1-enable (PF7-PF0 = 00011111)
+    GPIO_PORTF_DIR_R = 0x0e;                // Set output/input, 1-output (PF7-PF0 = 00001110)
+    GPIO_PORTF_PUR_R = 0x11;                // Enable pull-up resistor, 1-enable (PF7-PF0 = 00010001)
+    GPIO_PORTF_DATA_R = 0x00;               // Reset the data register (PF7-PF0 = 00000000)
 
-    GPIO_PORTF_ICR_R = 0x11;                // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 00010001)
+}
+void GPIOinterrupt_Initialisation(void){
+
+     // Setting PORTF interrupt registers
+     GPIO_PORTF_IS_R = 0x00;                // Interrupt sense, 1-level, 0-edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_IBE_R = 0x00;               // Interrupt both edge, 1-both edge, 0-single edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_IEV_R = 0x00;               // Interrupt event, 0-falling edge, 1-falling edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_ICR_R = 0xff;               // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 11111111)
+     GPIO_PORTF_IM_R = 0x11;                // Interrupt mask, 1-Enable interrupt (PF7-PF0 = 00010001)
+
+     NVIC_PRI7_R &= 0xFF3FFFFF;             // Prioritize and enable interrupts in NVIC
+
+     // Enable the GPIO Port F interrupt in the NVIC
+     NVIC_EN0_R = 0x40000000;               // Port F is interrupt 23 in the NVIC port is in 31st position
+
+     //__asm("    cpsie i");                // Global interrupt enable
+
 }
 
 void PWMConfiguration(void)
@@ -58,6 +71,18 @@ void PWMConfiguration(void)
     PWM1_3_CMPA_R = (duty/100)*time_period - 1;
     PWM1_3_CTL_R |= 0x01;                   // Enable generator 3 counter
     PWM1_ENABLE_R |= 0x040;                 // Enable PWM1 channel 6 Output
+}
+
+void main(void)
+
+    {
+    Sys_Port_Initialisation();
+    GPIOinterrupt_Initialisation();
+    PWMConfiguration();
+    PWM1_3_CMPA_R = (time_period * duty) / 100;     // Initial calculation of compare value @ 50 percent duty cycle
+    while(1){
+
+    }
 }
 
 void GPIOFHandler(void)
@@ -110,3 +135,4 @@ void button_read (){
         }
     }
 }
+
