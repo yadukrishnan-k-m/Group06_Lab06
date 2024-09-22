621418fe72bd0c2d381556d159926b3efc6082c4
Ravi <raviprakashboss87@gmail.com>
2024-09-22 14:48:20 +0530

    //GPIO interrupt added

--- Group06_Part01/main.c
+++ Group06_Part01/main.c
@@ -25,3 +25,21 @@ void PWMConfiguration(void)
     PWM1_3_CTL_R |= 0x01;                   // Enable generator 3 counter
     PWM1_ENABLE_R |= 0x040;                 // Enable PWM1 channel 6 Output
 }
+
+void GPIOinterrupt_Initialisation(void){
+
+     // Setting PORTF interrupt registers
+     GPIO_PORTF_IS_R = 0x00;                // Interrupt sense, 1-level, 0-edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_IBE_R = 0x00;               // Interrupt both edge, 1-both edge, 0-single edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_IEV_R = 0x00;               // Interrupt event, 0-falling edge, 1-falling edge (PF7-PF0 = 00000000)
+     GPIO_PORTF_ICR_R = 0xff;               // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 11111111)
+     GPIO_PORTF_IM_R = 0x11;                // Interrupt mask, 1-Enable interrupt (PF7-PF0 = 00010001)
+
+     NVIC_PRI7_R &= 0xFF3FFFFF;             // Prioritize and enable interrupts in NVIC
+
+     // Enable the GPIO Port F interrupt in the NVIC
+     NVIC_EN0_R = 0x40000000;               // Port F is interrupt 23 in the NVIC port is in 31st position
+
+     //__asm("    cpsie i");                // Global interrupt enable
+
+}
