b2bec6984b92cbb9a40e4215ffe8a9d08d65bc0a
Ravi <raviprakashboss87@gmail.com>
2024-09-22 15:18:46 +0530

    //Final Project 01 of 6th lab

--- Group06_Part01/main.c
+++ Group06_Part01/main.c
@@ -65,3 +65,36 @@ void GPIOinterrupt_Initialisation(void){
      //__asm("    cpsie i");                // Global interrupt enable
 
 }
+
+void main(void)
+
+{
+    Sys_Port_Initialisation();
+    GPIOinterrupt_Initialisation();
+    PWMConfiguration();
+    PWM1_3_CMPA_R = (time_period * duty) / 100;     // Initial calculation of compare value @ 50 percent duty cycle
+    while(1){
+
+    }
+}
+
+void GPIOFHandler(void)
+{
+    if(GPIO_PORTF_MIS_R == 0x10){           // Interrupt from SW1
+        //GPIO_PORTF_DATA_R ^= 0x08;          // Toggle data in PF1 (green led)
+        if(duty != 100)duty = duty+5;       // Increasing duty cycle by 5 %
+        if(duty > 100) duty = 100;
+        if(duty == 100) duty = 99;
+        PWM1_3_CMPA_R = (time_period * duty) / 100;
+    }
+
+    if(GPIO_PORTF_MIS_R == 0x01){           // Interrupt from SW2
+        //GPIO_PORTF_DATA_R ^= 0x02;          // Toggle data in PF1 (red led)
+        if(duty != 0) duty = duty-5;        // Decreasing duty cycle by 5 %
+        if(duty < 0) duty = 0;
+        PWM1_3_CMPA_R = (time_period * duty) / 100;
+    }
+
+    GPIO_PORTF_ICR_R = 0x11;                // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 00010001)
+}
+
